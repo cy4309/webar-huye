fd09cbdfe561d31fb187b9f84c3f508996f7e603
chester <chester@spe3d.co>
2025-08-19 16:58:48 +0800

    fix: craco issue

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 
+const isDev = process.env.NODE_ENV === "development";
+
 module.exports = {
   devServer: {
     host: "0.0.0.0",
     port: 5173,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "cert/server.key")),
-      cert: fs.readFileSync(path.resolve(__dirname, "cert/server.crt")),
-    },
+    ...(isDev && {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, "cert/server.key")),
+        cert: fs.readFileSync(path.resolve(__dirname, "cert/server.crt")),
+      },
+    }),
   },
   webpack: {
     alias: {
