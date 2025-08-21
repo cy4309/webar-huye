3196f03427d844eff5ce70274dbb443ee0b79d5a
chester <chester@spe3d.co>
2025-08-21 19:01:16 +0800

    add: test mdfk usdz

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,7 +30,7 @@ const Home: React.FC = () => {
           <model-viewer
             ref={mvRef}
             src="/models/ani-test2.glb"
-            ios-src="/models/ani-test2.usdz"
+            ios-src="/models/mdfk.usdz"
             ar
             ar-modes="scene-viewer webxr quick-look"
             camera-controls
@@ -57,7 +57,7 @@ const Home: React.FC = () => {
                     const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
                     if (isIOS) {
                       // 直接開 Quick Look 連結
-                      window.location.href = "/models/ani-test2.usdz";
+                      window.location.href = "/models/mdfk.usdz";
                     } else {
                       // 直接開 Scene Viewer intent（保險做法，可留著）
                       const glb = encodeURIComponent(
